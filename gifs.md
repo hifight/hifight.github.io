147c92f2a57c0089abbd9cbd97247331ca31de03
hifight <hifight.th@gmail.com>
2018-05-19 16:46:03 +0900

    add shuffle button

--- gifs.md
+++ gifs.md
@@ -9,11 +9,18 @@ permalink: /gifs/
 <div id="gfy-list">
 </div>
 
-Refresh to shuffle GIFs
+<script>
+function refreshFunction() {
+    location.reload();
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+</script>
 
-If you have any suggestions or any gif you want me to add, 
-please DM <a href="https://twitter.com/HiFightTH">@HiFightTH</a>.
+<button onclick="refreshFunction()" id="refreshButton">Shuffle!</button>
 
+If you have any suggestions or gif you want to add to this collections, 
+feel free to contact me at <a href="https://twitter.com/HiFightTH">@HiFightTH</a>.
 
 <script src="{{ site.baseurl }}/static/js/gifs_page.js"></script>
 	
--- static/js/gifs_page.js
+++ static/js/gifs_page.js
@@ -1,48 +1,61 @@
 ---
 ---
-(function() {
-   
-		var gfyArray = {{ site.data.gifs | jsonify }};
+// get gif data and shuffle the array
+var gfyArray = {{ site.data.gifs | jsonify }};
+var shuffledGfyArray = shuffle(gfyArray);
+var index = 0;
+var max = 5;
 
-		function shuffle(sourceArray) {
-			for (var i = 0; i < sourceArray.length - 1; i++) {
-				var j = i + Math.floor(Math.random() * (sourceArray.length - i));
+// add gfy on load
+addGfy()
 
-				var temp = sourceArray[j];
-				sourceArray[j] = sourceArray[i];
-				sourceArray[i] = temp;
-			}
-			return sourceArray;
-		}
+function shuffle(sourceArray) {
+	for (var i = 0; i < sourceArray.length - 1; i++) {
+		var j = i + Math.floor(Math.random() * (sourceArray.length - i));
 
-		function addGfyData(parentDiv, gfyData)
-		{
-			var figure = document.createElement("FIGURE");
-			figure.className = "entry-gfy";
-			parentDiv.appendChild(figure);
-			
-			var gfy = document.createElement("div");
-			gfy.className = "gfyitem";
-			gfy.setAttribute('data-expand', "true");
-			gfy.setAttribute('data-autoplay', "true");
-			gfy.setAttribute('data-id', gfyData.gfyid);
-			figure.appendChild(gfy);
-			
-			var gfyName = document.createElement("div");
-			gfyName.className = "gfy-name";
-			gfyName.innerHTML = '"' + gfyData.name + '"';
-			parentDiv.appendChild(gfyName);
-			
-			var br = document.createElement("br");
-			parentDiv.appendChild(br);
-		}
+		var temp = sourceArray[j];
+		sourceArray[j] = sourceArray[i];
+		sourceArray[i] = temp;
+	}
+	return sourceArray;
+}
 
-		var shuffledGfyArray = shuffle(gfyArray);
-		var div = document.getElementById('gfy-list');
-		var i;
-		for(i = 0; i < 5; i++)
-		{
-			addGfyData(div, shuffledGfyArray[i]);
-		}
-		
-})();
+function addGfyData(parentDiv, gfyData)
+{
+	var figure = document.createElement("FIGURE");
+	figure.className = "entry-gfy";
+	parentDiv.appendChild(figure);
+	
+	var gfy = document.createElement("div");
+	gfy.className = "gfyitem";
+	gfy.setAttribute('data-expand', "true");
+	gfy.setAttribute('data-autoplay', "true");
+	gfy.setAttribute('data-id', gfyData.gfyid);
+	figure.appendChild(gfy);
+	
+	var gfyName = document.createElement("div");
+	gfyName.className = "gfy-name";
+	gfyName.innerHTML = '"' + gfyData.name + '"';
+	parentDiv.appendChild(gfyName);
+	
+	var br = document.createElement("br");
+	parentDiv.appendChild(br);
+	
+}
+
+function addGfy()
+{
+	var div = document.getElementById('gfy-list');
+	
+	// while(div.firstChild){
+		// div.removeChild(div.firstChild);
+	// }
+
+	var i;
+	for(i = index; i < index + max; i++)
+	{
+		addGfyData(div, shuffledGfyArray[i]);
+	}
+	index = i;
+	
+}
